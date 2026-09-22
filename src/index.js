73437dd10467e6386ef2852d0b522972bb61f277
Antooon32 <antonskripka11@gmail.com>
2026-09-22 15:56:45 +0300

    lesson-1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Fragment } from "react";
+import React from 'react'; //! 1 - React-елементи
+import ReactDOM from 'react-dom/client'; //! 2 - Render React-елемента
+//! 1 - React-елементи
+// const element = React.createElement("div");
+// * document.createElement("div"); //* JS
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const element = React.createElement("div", { a: 5, b: 10 }, "Привіт  світ!");
+
+// const element = React.createElement(
+//   "div",
+//   { a: 5, b: 10 },
+//   "Привіт ",
+//   " ",
+//   "світ!"
+// );
+
+// const element = React.createElement("div", {
+//   a: 5,
+//   b: 10,
+//   children: ["Привіт ", " ", "світ!"]
+// });
+
+// //! 2 - Рендер одного React-елемента
+// //? ReactDOM.createRoot(document.getElementById('root')).render(element);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(element);
+// //* root.append(element) //* JS
+
+
+// console.log("element:", element);
+
+//! 3 - Рендер кількох React-елементів
+// const elem1 = React.createElement("span", { children: "Привіт " });
+// const elem2 = React.createElement("span", { children: "світ!" });
+
+//todo: Each child in a list should have a unique "key" prop
+// const elem1 = React.createElement("span", { key: "elem-1", children: "Привіт " });
+// const elem2 = React.createElement("span", { key: "elem-2", children: "світ!" });
+
+// const element = React.createElement("div", {
+//   a: 5,
+//   b: 10,
+//   children: [elem1, " ", elem2]
+// });
+
+// console.log("element:", element);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(element);
+
+//! 4 - Рендер JSX-елемента (XML-образний синтаксис)
+// const element = React.createElement("div", {
+//   a: 5,
+//   b: 10,
+//   children: "Привіт, світ!",
+// });
+
+// console.log("element:", element);
+
+// const jsxElement = <div>Привіт, мій світ</div>
+// const jsxElementInput = <img src='https://upload.wikimedia.org/wikipedia/commons/6/66/AH-64D_Apache_Longbow.jpg?utm_source=ru.wikipedia.org&utm_campaign=index&utm_content=original' alt='123'/>
+// console.log("jsxElement:", jsxElement);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// // root.render(element);
+// root.render(jsxElementInput);
+
+//! 5 - Рендер Вкладених JSX-елементів (інтерполяція)
+const elem1 = <span>Привіт  </span>;
+const elem2 = <span>світ!</span>;
+
+const element = (
+  // <Fragment>
+  //   {elem1}
+  //   {elem2}
+  //   {"!!"}
+  // </Fragment>
+  <>
+    {elem1}
+    {elem2}
+    {"!!"}
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log("element:", element);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
